74f0e99112d754449029416b560ff76baac193ce
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-05-05 11:57:06 -0700

    - some work to get typescript happy

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 import BlogCard from "@/components/BlogCard";
 import MDXContent from "@/helpers/MDXContent";
-import Pagination from "@/components/Pagination";
+// import Pagination from "@/components/Pagination";
 import config from "@/config/config.json";
 import { getListPage, getSinglePage } from "@/lib/contentParser";
 import { getAllTaxonomy, getTaxonomy } from "@/lib/taxonomyParser";
--- src/layouts/helpers/MDXContent.tsx
+++ src/layouts/helpers/MDXContent.tsx
@@ -1,5 +1,6 @@
 import shortcodes from "@/shortcodes/all";
 import { MDXRemote } from "next-mdx-remote/rsc";
+
 import remarkGfm from "remark-gfm";
 
 const MDXContent = ({ content }: { content: any }) => {
