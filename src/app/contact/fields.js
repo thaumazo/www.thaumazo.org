06705936bfe6eadae99c8ea3fce56d7a88b5e0d9
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-05-07 10:57:38 -0700

    - some additional work on the contact form

--- src/app/contact/fields.js
+++ src/app/contact/fields.js
@@ -18,12 +18,41 @@ const fields = {
     field: "textarea",
     rows: 4,
   },
+  interest: {
+    label: "I'm Interested In",
+    required: true,
+    field: "checkboxlist",
+    options: [
+      ["event", "Attending a Thaumazo Event"],
+      ["joining", "Joining the Thaumazo Community"],
+      ["volunteering", "Volunteering at Thaumazo"],
+      ["partnering", "Partnering with Thaumazo on a project"],
+      ["finding", "Finding out more about a specific project / initiative"],
+      ["support", "Supporting Thaumazo's Work"],
+      ["telling", "Telling Thaumazo about a Person, Organization, or Project of interest"],
+      ["opportunity", "Discussing an opportunity with Thaumazo"],
+    ]
+  },
   details: {
     label: "Any Further Details",
-    required: true,
     field: "textarea",
     rows: 4,
   },
+  project: {
+    label: "I'd like to find out about / discuss",
+    field: "checkboxlist",
+    options: [
+      ["StoryTime", "StoryTime"],
+      ["CommunityMycelium", "Community Mycelium"],
+      ["CrisisForge", "CrisisForge"],
+      ["MakeAChange", "Make a Change"],
+      ["AIAndUsEvents", "AI & Us Events"],
+      ["AIAndUsWorkingGroup", "AI & Us Working Group"],
+      ["StewardsOfTomorrow", "Stewards of Tomorrow"],
+      ["PersonalPathways", "Personal Pathways"],
+   ]
+  },
+
 };
 
 export default fields;
--- src/app/contact/formAction.js
+++ src/app/contact/formAction.js
@@ -1,5 +1,19 @@
 "use server";
 
-export default async function formAction() {
-  // console.log("foo bar");
+import checkServerValidity from "@thaumazo/forms/validity/checkServerValidity";
+import fields from "./fields";
+
+export default async function formAction(state, formData) {
+
+  const fieldErrors = checkServerValidity(fields, formData);
+  if (fieldErrors) {
+    return {
+      error:
+        "We couldn't process your request. See the errors marked in red below.",
+      fieldErrors,
+    };
+  }
+
+  return { success: "Thank you for reaching out. We look forward to reviewing your submission." };
+
 }
