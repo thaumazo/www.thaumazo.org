817927aada13531053ee96a8ed2825667b6aa3c2
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-09-26 20:12:39 -0700

    Added generateStaticParams to dynamic routes. This should generate all the pages at build time avoid the 404 problem at vercel.

--- src/app/community/[slug]/page.jsx
+++ src/app/community/[slug]/page.jsx
@@ -4,6 +4,13 @@ import Post from "@/components/Post";
 import loadMD from "@/utils/loadMD";
 import { notFound } from "next/navigation";
 
+import listMD from "@/utils/listMD";
+export async function generateStaticParams() {
+  let list = await listMD("community/people");
+  return list.map((v) => ({ slug: v.slug }));
+}
+export const dynamicParams = false;
+
 export default async function Page({ params: { slug } }) {
   const data = await loadMD("community/people/" + slug);
   if (data === false) {
--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -4,6 +4,13 @@ import Post from "@/components/Post";
 import loadMD from "@/utils/loadMD";
 import { notFound } from "next/navigation";
 
+import listMD from "@/utils/listMD";
+export async function generateStaticParams() {
+  let list = await listMD("projects/posts");
+  return list.map((v) => ({ slug: v.slug }));
+}
+export const dynamicParams = false;
+
 export default async function Page({ params: { slug } }) {
   const data = await loadMD("projects/posts/" + slug);
 
