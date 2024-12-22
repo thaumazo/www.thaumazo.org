db6b079a16f0fcff70b1b9d42acea69dd445d0a2
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-12-21 19:34:59 -0800

    Added images to the Organization list

--- src/app/organizations/Posts.jsx
+++ src/app/organizations/Posts.jsx
@@ -1,18 +1,32 @@
 import listMD from "@/utils/listMD";
 import Link from "next/link";
 
+import Image from "next/Image";
+
 export default async function Posts() {
   let postData = await listMD("organizations/posts");
   const posts = postData.sort((a, b) => a.title.localeCompare(b.title));
 
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 max-w-3xl mx-auto my-8">
-      {posts.map(({ title, slug }) => {
+      {posts.map(({ title, slug, image }) => {
         const link = "/organizations/" + slug;
         return (
-          <div key={slug} className="flex justify-center row">
-            <Link className="flex justify-center" href={link}>
-              {title}
+          <div key={slug} className="flex justify-center">
+            <Link
+              className="flex flex-col items-center justify-center"
+              href={link}
+            >
+              {image ? (
+                <Image
+                  {...image}
+                  alt=""
+                  className="w-auto h-auto w-full"
+                  sizes="128px"
+                  priority
+                />
+              ) : null}
+              <span>{title}</span>
             </Link>
           </div>
         );
