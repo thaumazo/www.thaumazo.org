59ab44de4ca186bdc7a2c4cd817f26d30eef1b03
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-12-22 15:25:12 -0800

    Added image to Projects list

--- src/app/projects/Posts.jsx
+++ src/app/projects/Posts.jsx
@@ -1,17 +1,32 @@
+import React from "react";
 import listMD from "@/utils/listMD";
 import Link from "next/link";
+
+import Image from "next/image";
 
 export default async function Posts() {
   let postData = await listMD("projects/posts");
   const posts = postData.sort((a, b) => a.title.localeCompare(b.title));
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-3xl mx-auto my-8">
-      {posts.map(({ title, slug }) => {
+    <div className="grid grid-cols-1 lg:grid-cols-1 gap-4 max-w-3xl mx-auto my-8">
+      {posts.map(({ title, slug, image }) => {
         const link = "/projects/" + slug;
         return (
-          <div key={slug} className="flex justify-left row">
-            <Link className="flex justify-left" href={link}>
+          <div key={slug}>
+            <Link className="flex gap-4 justify-left items-center" href={link}>
+              <span className="block w-32">
+                {image ? (
+                  <Image
+                    {...image}
+                    alt=""
+                    className="w-auto h-auto max-h-32"
+                    sizes="128px"
+                    priority
+                  />
+                ) : null}
+              </span>
+
               {title}
             </Link>
           </div>
