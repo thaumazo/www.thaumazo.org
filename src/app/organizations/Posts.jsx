7b2570bc0efb8ee746db334d4d432fff3fd32f45
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-12-22 14:56:00 -0800

    Improved the look of the organizations list

--- src/app/organizations/Posts.jsx
+++ src/app/organizations/Posts.jsx
@@ -8,20 +8,20 @@ export default async function Posts() {
   const posts = postData.sort((a, b) => a.title.localeCompare(b.title));
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 max-w-3xl mx-auto my-8">
+    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 auto-rows-[1fr] max-w-3xl mx-auto my-8">
       {posts.map(({ title, slug, image }) => {
         const link = "/organizations/" + slug;
         return (
           <div key={slug} className="flex justify-center">
             <Link
-              className="flex flex-col items-center justify-center"
+              className="flex flex-col items-center justify-center gap-2 w-full"
               href={link}
             >
               {image ? (
                 <Image
                   {...image}
                   alt=""
-                  className="w-auto h-auto w-full"
+                  className="w-auto h-auto max-h-32"
                   sizes="128px"
                   priority
                 />
