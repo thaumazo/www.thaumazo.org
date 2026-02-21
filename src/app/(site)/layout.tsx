69dd53590594a782f5afba5bff746bcf30c759ae
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2026-02-21 09:19:25 -0800

    Added Google Analytics

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import RecaptchaProvider from "@kenstack/context/RecaptchaProvider";
 // import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import type { Metadata } from "next";
 export const metadata: Metadata = {
@@ -13,11 +14,14 @@ export const metadata: Metadata = {
 
 export default async function SiteLayout({ children }) {
   return (
-    <RecaptchaProvider>
-      <Header />
-      {children}
-      <Footer />
-      {/* <GoogleAnalytics gaId="G-WC8VW2K63D" /> */}
-    </RecaptchaProvider>
+    <>
+      <RecaptchaProvider>
+        <Header />
+        {children}
+        <Footer />
+        {/* <GoogleAnalytics gaId="G-WC8VW2K63D" /> */}
+      </RecaptchaProvider>
+      <GoogleAnalytics gaId="G-XV6M6H54V2" />
+    </>
   );
 }
