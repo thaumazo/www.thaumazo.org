8a13e2d79e9ddd2589b1d29389907510d4091ea7
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2026-05-28 16:54:13 -0700

    Put Thaumazo logo back on top of admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@kenstack/admin/Sidebar";
 import QueryProvider from "@kenstack/context/QueryProvider";
 import admin from "@/modules/admin";
 import AccountMenu from "@/components/AccountMenu";
-import Logo from "@kenstack/admin/Sidebar/KenstackLogo";
+import Image from "next/image";
 
 export default function AdminLayout({
   children,
@@ -12,7 +12,16 @@ export default function AdminLayout({
   return (
     <QueryProvider>
       <Sidebar
-        logo={<Logo className="block h-8 w-auto" />}
+        logo={
+          <Image
+            src="/images/logo/thaumazo-symbol-beside.svg"
+            alt="Thaumazo"
+            width={1343}
+            height={347}
+            className="block h-8 w-auto"
+            priority
+          />
+        }
         admin={admin}
         accountMenu={<AccountMenu fallback={null} />}
       >
